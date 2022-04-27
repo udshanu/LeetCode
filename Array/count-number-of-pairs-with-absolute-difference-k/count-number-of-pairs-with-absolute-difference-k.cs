1dfce4ac5716d43022812c8142bba36ef9772745
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2022-04-27 23:57:13 +0530

    Create count-number-of-pairs-with-absolute-difference-k.cs

---
+++ Array/count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int CountKDifference(int[] nums, int k) {
+        int count = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (Math.Abs(nums[i] - nums[j]) == k)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
